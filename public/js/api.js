4119a1ca5087f803c27c7cd7f455ec316ea7fa12
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2023-02-08 12:02:22 +0530

    netflix clone

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "68d6ae00f051bb720e27905342ac4673";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
